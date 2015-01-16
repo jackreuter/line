5ebe59b33e721d44bd1385a5191aa29e24eb008f
Jack Reuter <jreuter@wesleyan.edu>
2015-01-16 15:47:16 +0000

    500 bug fixed

--- line/settings/base.py
+++ line/settings/base.py
@@ -33,7 +33,7 @@ import dj_database_url
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = DEBUG
 
