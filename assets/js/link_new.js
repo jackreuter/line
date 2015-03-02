3857c5be30e89db18e95fcac1ae9c99f2c7779bf
pate <csanders@wesleyan.edu>
2015-03-02 00:05:58 +0000

    embedly

---
+++ assets/js/link_new.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('#trigger-title').click(function() {
+		$('#input-title').slideToggle('slow');
+	});
+	$('#trigger-share').click(function() {
+		$('#share-post').slideToggle('slow');
+	})
+	});
