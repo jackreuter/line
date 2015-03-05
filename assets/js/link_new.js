624e5577e68310b6baf0b142bfb7ae43fb86f67a
pate <csanders@wesleyan.edu>
2015-03-05 22:15:31 +0000

    6 tags got me feelin like pac boi

--- assets/js/link_new.js
+++ assets/js/link_new.js
@@ -2,7 +2,22 @@ $(document).ready(function() {
 	$('#trigger-title').click(function() {
 		$('#input-title').slideToggle('slow');
 	});
-	$('#trigger-share').click(function() {
-		$('#share-post').slideToggle('slow');
-	})
+	$('#trigger-share-1').click(function() {
+		$('#share-post-1').slideToggle('slow');
+	});
+	$('#trigger-share-2').click(function() {
+		$('#share-post-2').slideToggle('slow');
+	});
+	$('#trigger-share-3').click(function() {
+		$('#share-post-3').slideToggle('slow');
+	});
+	$('#trigger-share-4').click(function() {
+		$('#share-post-4').slideToggle('slow');
+	});
+	$('#trigger-share-5').click(function() {
+		$('#share-post-5').slideToggle('slow');
+	});
+	$('#trigger-share-6').click(function() {
+		$('#share-post-6').slideToggle('slow');
+	});
 	});
