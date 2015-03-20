2bcd9a2720e4d59faa7401e4843cd964196d575d
pate <csanders@wesleyan.edu>
2015-03-20 09:23:33 +0000

    jquery repost tags

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,25 @@
 
 $(document).ready(function() {
   $('#summary-button').click(function() {
-    $('#summary').slideToggle('slow');
+    $(this).next('#summary').slideToggle('slow');
+  });
+  $('#plus').click(function() {
+  	$('#share-post-0').slideToggle('slow');
+  });
+   $('#share-post-0').click(function() {
+  	$('#share-post-1').slideToggle('slow');
+  });
+    $('#share-post-1').click(function() {
+  	$('#share-post-2').slideToggle('slow');
+  });
+     $('#share-post-2').click(function() {
+  	$('#share-post-3').slideToggle('slow');
+  });
+      $('#share-post-3').click(function() {
+  	$('#share-post-4').slideToggle('slow');
+  });
+       $('#share-post-4').click(function() {
+  	$('#share-post-5').slideToggle('slow');
   });
   $('[data-toggle=tooltip]').tooltip();
 });
