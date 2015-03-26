8fad8194a9e37d53b59572730bcb6066e6896a3e
pate <csanders@wesleyan.edu>
2015-03-26 05:40:08 +0000

    embedly jquery, repost jquery, posting links is in modal on base layer now but SOME THINGS DONT WORK

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,30 +1,40 @@
 
 $(document).ready(function() {
-  $(".summary-button").each(function(){
-  $(this).click(function() {
-    $(this).next('#summary').slideToggle('slow');
-  });
+  $(".summary-button").each(function() {
+     $(this).click(function() {
+    $(this).next().slideToggle('slow');
 });
-  $('.plus').click(function() {
+   });
+  $('.plus').each(function() {
     $(this).click(function() {
-  	$('#share-post-0').slideToggle('slow');
+  	$(this).next().slideToggle('slow');
   });
   });
-   $('#share-post-0').click(function() {
-  	$('#share-post-1').slideToggle('slow');
-  });
-    $('#share-post-1').click(function() {
-  	$('#share-post-2').slideToggle('slow');
-  });
-     $('#share-post-2').click(function() {
-  	$('#share-post-3').slideToggle('slow');
-  });
-      $('#share-post-3').click(function() {
-  	$('#share-post-4').slideToggle('slow');
-  });
-       $('#share-post-4').click(function() {
-  	$('#share-post-5').slideToggle('slow');
-  });
+  //  $('.trigger-share-1').each(function() {
+  //    $(this).click(function() {
+  //   $(this).parent().css("background color", "red");
+  //   });
+  // });
+  //   $('.share-post-1').click(function() {
+  // 	 $(this).click(function() {
+  //   $(this).next().slideToggle('slow');
+  //   });
+  // });
+  //    $('.share-post-2').click(function() {
+  // 	 $(this).click(function() {
+  //   $(this).next().slideToggle('slow');
+  //   });
+  // });
+  //     $('.share-post-3').click(function() {
+  // 	 $(this).click(function() {
+  //   $(this).next().slideToggle('slow');
+  //   });
+  // });
+  //      $('.share-post-4').click(function() {
+  // 	$(this).click(function() {
+  //   $(this).next().slideToggle('slow');
+  // });
+  // });
   $('[data-toggle=tooltip]').tooltip();
 });
 
