d1a9aa2e1a7e8a11e710a5da76a2ef40bddd6ebe
pate <csanders@wesleyan.edu>
2015-03-27 21:46:48 +0000

    updated files

---
+++ assets/js/hot.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  $(".summary-button").each(function() {
+     $(this).click(function() {
+    $(this).next().slideToggle('slow');
+});
+   });
+  $('.plus').each(function() {
+    $(this).click(function() {
+  	$(this).next().slideToggle('slow');
+  });
+  });
+  //  $('.trigger-share-1').each(function() {
+  //    $(this).click(function() {
+  //   $(this).parent().css("background color", "red");
+  //   });
+  // });
+  //   $('.share-post-1').click(function() {
+  // 	 $(this).click(function() {
+  //   $(this).next().slideToggle('slow');
+  //   });
+  // });
+  //    $('.share-post-2').click(function() {
+  // 	 $(this).click(function() {
+  //   $(this).next().slideToggle('slow');
+  //   });
+  // });
+  //     $('.share-post-3').click(function() {
+  // 	 $(this).click(function() {
+  //   $(this).next().slideToggle('slow');
+  //   });
+  // });
+  //      $('.share-post-4').click(function() {
+  // 	$(this).click(function() {
+  //   $(this).next().slideToggle('slow');
+  // });
+  // });
+  $('[data-toggle=tooltip]').tooltip();
+});
