50c68628361cb8169873fad62d84c6f660dc2b8f
Jack Reuter <jreuter@wesleyan.edu>
2015-04-01 17:01:10 +0000

    get_feedback command

--- feedback/management/commands/get_feedback.py
+++ feedback/management/commands/get_feedback.py
@@ -6,5 +6,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         all_entries = Feedback.objects.all()
-        for entry in all_entries:
-            self.stdout.write('entry "%s"' % entry)
+        for index,entry in enumerate(all_entries):
+            self.stdout.write('%s "%s" %s' % (index, entry.text, entry.posted_by))
